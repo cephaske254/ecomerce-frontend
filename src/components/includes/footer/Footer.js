11e78ff0f68c0ff1927c49e534f616d2fc525bfa
cephaske254 <cephaske254@gmail.com>
2021-02-09 17:08:53 +0300

    add social links

--- src/components/includes/footer/Footer.js
+++ src/components/includes/footer/Footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
           <div className="row justify-content-center align-items-center">
             <div className="text-center">
               <div className="m-0 small d-flex flex-column">
-                <small>Development Version</small>
+                {/* <small>Development Version</small> */}
                 <div className="m-0">
                   <a
                     target="_blank"
@@ -23,6 +23,32 @@ class Footer extends React.Component {
                   &copy; 2020
                 </div>
                 <small>Cephas Too</small>
+                <div className="d-flex justify-content-center">
+                 
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/cephastoo/"
+                    className="btn btn-sm text-light"
+                  >
+                    <i className="fab fa-linkedin fa-lg"></i>
+                  </a> <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://api.whatsapp.com/send?phone=254702149456"
+                    className="btn btn-sm text-light"
+                  >
+                    <i className="fab fa-whatsapp fa-lg"></i>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.github.com/cephaske254/"
+                    className="btn btn-sm text-light"
+                  >
+                    <i className="fab fa-github fa-lg"></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
